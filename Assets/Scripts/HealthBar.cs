20326187e416c733fecdecbd6b404bc675deb7da
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2019-10-19 02:26:20 +0200

    Gameover (Added) + Animation (HealthBar)
    Particle system Added (but just in the menu, player now can die. animation just in menu

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -8,25 +8,43 @@ public class HealthBar : MonoBehaviour
     public float maxHealth;
     public float currentHealth;
     public float health;
+    private float healing;
+    private float lol;
+    private float cronometro;
     public Image hpImage;
     public Text hpText;
+    public GameObject player;
 
 
     private void Start()
     {
         hpImage.GetComponent<Image>().fillAmount = 1;
         currentHealth = maxHealth;
+        healing = maxHealth;
+        cronometro = 0;
     }
 
     public void Damage(float damageAmount)
     {
+        //currentHealth -= damageAmount;
+        //if (currentHealth != healing && cronometro !=damageAmount)
+        //{
+        //    cronometro += 1f;
+        //    healing -= 1f;
+        //    healing = lol;
+        //    lol= (lol - damageAmount) / maxHealth;
+        //    hpImage.GetComponent<Image>().fillAmount = lol;
+        //    Debug.Log(lol + "lol");
+        //    Debug.Log(healing + );
+        //    Debug.Log(cronometro);
+        //}
         health = (currentHealth - damageAmount) / maxHealth;
-        currentHealth = currentHealth -damageAmount;
+        currentHealth = currentHealth - damageAmount;
         hpImage.GetComponent<Image>().fillAmount = health;
         hpText.GetComponent<Text>().text = currentHealth.ToString() + "%";
         if (health <= 0)
         {
-            hpImage.GetComponent<Text>().text = "0";
+            Destroy(player);
         }
     }
 }
