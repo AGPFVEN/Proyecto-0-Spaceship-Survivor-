9bd239731e997709a9e58241fbc97b02af4eaf6c
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2019-10-20 04:24:52 +0200

    Update healthbar
     Trying to animate it

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -5,11 +5,11 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    private float lol;
     public float maxHealth;
     public float currentHealth;
     public float health;
     private float healing;
-    private float lol;
     private float cronometro;
     public Image hpImage;
     public Text hpText;
@@ -22,25 +22,32 @@ public class HealthBar : MonoBehaviour
         currentHealth = maxHealth;
         healing = maxHealth;
         cronometro = 0;
+        lol = 1;
     }
 
     public void Damage(float damageAmount)
     {
         //currentHealth -= damageAmount;
-        //if (currentHealth != healing && cronometro !=damageAmount)
+        //if (currentHealth != healing && cronometro != damageAmount)
         //{
-        //    cronometro += 1f;
-        //    healing -= 1f;
-        //    healing = lol;
-        //    lol= (lol - damageAmount) / maxHealth;
+        //    cronometro += 1f * Time.deltaTime;
+        //    healing -= 1f * Time.deltaTime;
+        //    healing = lol * Time.deltaTime;
+        //    lol = (lol - damageAmount) / maxHealth;
         //    hpImage.GetComponent<Image>().fillAmount = lol;
         //    Debug.Log(lol + "lol");
-        //    Debug.Log(healing + );
-        //    Debug.Log(cronometro);
+        //    Debug.Log(healing + "healing");
+        //    Debug.Log(cronometro + "cronometro");
+        //    if (health <= 0)
+        //    {
+        //        Destroy(player);
+        //    }
         //}
+
         health = (currentHealth - damageAmount) / maxHealth;
+        health = lol;
         currentHealth = currentHealth - damageAmount;
-        hpImage.GetComponent<Image>().fillAmount = health;
+        hpImage.GetComponent<Image>().fillAmount = lol;
         hpText.GetComponent<Text>().text = currentHealth.ToString() + "%";
         if (health <= 0)
         {
