9f32d1ba425c00859a34200834e0cc6632204a68
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2019-11-02 04:09:55 +0100

    Camera Follow (Campo de pruebas)
    The camera follows the player

---
+++ Assets/Scripts/Campo de pruebas/FollowC.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowC : MonoBehaviour
+{
+    Transform Target;
+    public GameObject Player;
+    public float movementSpeed;
+    //Rigidbody2D rb;
+    public Vector2 lookDirection;
+    public float lookAngle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this.rb.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Rotation to look Player
+        Target = GameObject.FindGameObjectWithTag("Player").transform;
+        Debug.DrawLine(transform.position, Target.position);
+        lookDirection = Target.position - transform.position;
+        lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, lookAngle - 90f);
+
+        //Movimiento
+        transform.position = Vector2.MoveTowards(transform.position, Target.position, movementSpeed * Time.deltaTime);
+    }
+}
