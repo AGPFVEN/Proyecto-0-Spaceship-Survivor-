5641a04b122b778a1c9a988b04a384124353f30b
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2019-11-09 04:33:43 +0100

    Camera
    Now is the perfect camera

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -11,14 +11,21 @@ public class CameraMovement : MonoBehaviour
     //Rigidbody2D rb;
     public Vector2 lookDirection;
     public float lookAngle;
+    public float smoothSpeed = 1f;
+    public Vector3 offset;
     // Start is called before the first frame update
 
     void Update()
     {
         //Movimiento
         Raycasting();
-        transform.position = Vector2.MoveTowards(transform.position, Target.position, 0);
+        //transform.position = Vector2.MoveTowards(transform.position, Target.position, 0);
+        //new Vector2.Lerp();
+        Vector3 desiredPosition = Target.position + offset;
+        Vector3 SmoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = SmoothedPosition;
 
+        transform.position = SmoothedPosition;
     }
     void Raycasting()
     {
