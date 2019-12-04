7275b021c10e106d2444f2fbcf75ce6a3344b088
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2019-12-04 22:29:08 +0100

    re-doing player
    Well-Done, Now the player is like it was before

---
+++ Assets/Scripts/Campo de pruebas/TesterMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesterMove : MonoBehaviour
+{
+    public Vector2 lookDirection;
+    public float lookAngle;
+
+    void FixedUpdate()
+    {
+        lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, lookAngle - 90f);
+    }
+}
