67d7f8d51cef7724b2038105a85cb2cbcfb52a69
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2019-12-06 22:42:07 +0100

    Daily update

--- Assets/Scripts/Campo de pruebas/TesterMove.cs
+++ Assets/Scripts/Campo de pruebas/TesterMove.cs
@@ -6,11 +6,24 @@ public class TesterMove : MonoBehaviour
 {
     public Vector2 lookDirection;
     public float lookAngle;
+    public float speed;
+    public float tLimit;
 
     void FixedUpdate()
     {
-        lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, lookAngle - 90f);
+        ////Movimiento 
+        float vInput = Input.GetAxis("Vertical");
+        transform.position += new Vector3(0, vInput * speed * Time.deltaTime * Time.deltaTime, 0);
+        float hInput = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(hInput * speed * Time.deltaTime * Time.deltaTime, 0, 0);
+
+        //Aceleración
+        if (tLimit > 0)
+        {
+            tLimit -= Time.deltaTime;
+            tLimit -= Time.deltaTime;
+            tLimit -= Time.deltaTime;
+            speed = speed + 1 * Time.deltaTime;
+        }
     }
 }
