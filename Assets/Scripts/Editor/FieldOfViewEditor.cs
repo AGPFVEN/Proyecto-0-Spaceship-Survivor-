04f4d8aa4e13bf576ec3aaa07628eeace1ff88a4
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2019-12-11 00:39:44 +0100

    Enemy detection

--- Assets/Scripts/Editor/FieldOfViewEditor.cs
+++ Assets/Scripts/Editor/FieldOfViewEditor.cs
@@ -11,11 +11,6 @@ public class FieldOfViewEditor : Editor
         EnemyController fow = (EnemyController)target;
         Handles.color = Color.white;
         Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.left, 360, fow.viewRadius);
-        Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
-        Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
-
-        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
-        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
 
     }
 }
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -20,8 +20,12 @@ public class EnemyController : MonoBehaviour
     public float viewRadius;
     [Range(0, 360)]
     public float viewAngle;
-    public Vector3 DirFromAngle(float  angleInDegrees)
+    public Vector3 DirFromAngle(float  angleInDegrees, bool angleIsGlobal)
     {
+        if(!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
         return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
     }
 
