652acadb43a100adcd3324e4c83d8dd395a8ce6d
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2020-01-04 21:31:47 +0100

    New spawn algorithm

---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    //Crono
+    float crono;
+    float cronoL;
+
+    void Start()
+    {
+        crono = 0;
+        cronoL = 20;
+    }
+
+    void Update()
+    {
+        
+    }
+}
