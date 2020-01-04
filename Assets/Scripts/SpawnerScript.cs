0005dc5da7c58dd16e48cfd5ad2524fa344c6cab
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2020-01-04 23:20:54 +0100

    Doing a spawner algorithm

--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -4,14 +4,39 @@ using UnityEngine;
 
 public class SpawnerScript : MonoBehaviour
 {
+    //Enemys 
+    public GameObject NormalE;
+    //Prefab number
+    float spawnerNum;
+
     //Crono
     float crono;
     float cronoL;
 
+    //Prefabs
+    public GameObject spawnerDL;
+    public GameObject spawnerDR;
+    public GameObject spawnerUL;
+    public GameObject spawnerUR;
+
+    //Prefabs to number
+    GameObject[] spawner;
+
     void Start()
     {
+        //Crono (start)
         crono = 0;
         cronoL = 20;
+
+        //Prefabs to number (start)
+        spawner = new GameObject[3];
+        spawner[0] = spawnerDL;
+        spawner[1] = spawnerDR;
+        spawner[2] = spawnerUL;
+        spawner[3] = spawnerUR;
+
+        //Prefab num (start)
+        spawnerNum = Random.Range(0, 3);
     }
 
     void Update()
