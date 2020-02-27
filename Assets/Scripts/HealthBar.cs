49cb11334db4a1b230cf635b27bde6bde9d6ae5d
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2020-02-27 23:33:13 +0100

    daily update

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -5,12 +5,9 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    //private float lol;
     public float maxHealth;
     private float currentHealth;
     private float health;
-    //private float healing;
-    //private float cronometro;
     public Image hpImage;
     public Text hpText;
     public GameObject player;
@@ -19,30 +16,10 @@ public class HealthBar : MonoBehaviour
     {
         hpImage.fillAmount = 1;
         currentHealth = maxHealth;
-        //healing = maxHealth;
-        //cronometro = 0;
-        //lol = 1;
     }
 
     public void Damage(float damageAmount)
     {
-        //currentHealth -= damageAmount;
-        //if (currentHealth != healing && cronometro != damageAmount)
-        //{
-        //    cronometro += 1f * Time.deltaTime;
-        //    healing -= 1f * Time.deltaTime;
-        //    healing = lol * Time.deltaTime;
-        //    lol = (lol - damageAmount) / maxHealth;
-        //    hpImage.GetComponent<Image>().fillAmount = lol;
-        //    Debug.Log(lol + "lol");
-        //    Debug.Log(healing + "healing");
-        //    Debug.Log(cronometro + "cronometro");
-        //    if (health <= 0)
-        //    {
-        //        Destroy(player);
-        //    }
-        //}
-
         //Este es el que funciona
         health = (currentHealth - damageAmount) / maxHealth;
         currentHealth = currentHealth - damageAmount;
@@ -54,4 +31,3 @@ public class HealthBar : MonoBehaviour
         }
     }
 }
- 
