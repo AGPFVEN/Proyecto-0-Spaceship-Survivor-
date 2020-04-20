1ad47868bf5b587a5e781ccd3be4e584f50a311d
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2020-04-20 23:34:19 +0200

    Explosive Enemy

---
+++ Assets/Scripts/ExploEnemyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExploEnemyScript : MonoBehaviour
+{
+    //Movement
+    Transform target;
+    Rigidbody2D rb;
+    Vector2 movevector;
+    void Start()
+    {
+        //Movement Start
+        target = GameObject.FindWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        movevector = target.position-transform.position;
+        rb.AddForce(movevector/2 , ForceMode2D.Force);
+    }
+}
